f844bf154e048d4bfc48ddfc5ef11f82efa49eb9
Cocodilla <evgeniacesar@gmail.com>
2022-10-30 22:27:18 +0300

    first commit

---
+++ app/src/main/java/FirstApp/Training/MainActivity.java
@@ -0,0 +1,32 @@
+package FirstApp.Training;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import FirstApp.Training.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Hello android");
+textView.setTextSize(22);
+setContentView(textView);
+    }
+}
